c429634c7856cac876a9d39bde95f634e502e10f
Yoni Doron-Peters <jdoronpeters@gloo.us>
2020-11-10 22:39:00 -0700

    add pet gender selection. Hide options unless pet is selected

--- src/components/PetFinderApp.js
+++ src/components/PetFinderApp.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../logo.svg';
 
 export default class PetFinderApp extends React.Component {
   state = {
@@ -9,24 +8,38 @@ export default class PetFinderApp extends React.Component {
 
   handleSubmitForm = (e) => {
     e.preventDefault();
-    let dogs;
-    if (e.target.elements.dogs.checked) {
-      const dogBreeds = e.target.elements.dogBreed.value.trim();
-      dogs = dogBreeds.split(',').map(dog => dog.trim()).filter(Boolean);
-    }
-    let cats;
-    if (e.target.elements.cats.checked) {
-      const catBreeds = e.target.elements.catBreed.value.trim();
-      cats = catBreeds.split(',').map(cat => cat.trim()).filter(Boolean);
-    }
+    const dogs = this.getDogs(e);
+    const cats = this.getCatOptions(e);
     const phoneError = this.validatePhoneNumber(e);
     const optionSelectError = !cats && !dogs && 'You must select at least 1 option';
     this.setState(() => ({
         errors: [phoneError, optionSelectError].filter(Boolean),
         subscription: { dogs, cats }
       }),
-      this.handleAddOption);
+      this.handleAddNotification);
   };
+
+  getDogs(e) {
+    if (e.target.elements.dogs.checked) {
+      const gender = e.target.elements.dogGender.value || 'both';
+      const dogBreeds = e.target.elements.dogBreed.value.trim();
+      return {
+        gender,
+        breeds: dogBreeds.split(',').map(dog => dog.trim()).filter(Boolean)
+      };
+    }
+  }
+
+  getCatOptions(e) {
+    if (e.target.elements.cats.checked) {
+      const gender = e.target.elements.catGender.value || 'both';
+      const catBreeds = e.target.elements.catBreed.value.trim();
+      return {
+        gender,
+        breeds: catBreeds.split(',').map(cat => cat.trim()).filter(Boolean)
+      };
+    }
+  }
 
   validatePhoneNumber = (e) => {
     const phone = e.target.elements.phone.value;
@@ -39,7 +52,7 @@ export default class PetFinderApp extends React.Component {
     }
   };
 
-  handleAddOption = () => {
+  handleAddNotification = () => {
     if (!this.state.errors.length) {
       console.log(this.state.subscription);
     } else {
@@ -50,30 +63,58 @@ export default class PetFinderApp extends React.Component {
     }
   };
 
+  handleAnimalSelected = (e) => {
+    const animal = e.target.name;
+    const currentVal = document.getElementById(animal).hidden;
+    document.getElementById(animal).hidden = !currentVal;
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>Pet Finder</h1>
-          {/*<img src={logo} className="App-logo" alt="logo"/>*/}
           <p>
             Notify me for:
           </p>
           <form className="add-option" onSubmit={this.handleSubmitForm}>
-            <p>
-              <input type="checkbox" id="option1" name="cats"/>
-              <label htmlFor="option1"> Cats </label>
+            {/*checkbox*/}
+            <input type="checkbox" id="option1" name="cats" onChange={this.handleAnimalSelected}/>
+            <label htmlFor="option1"> Cats </label>
+            <div id="cats" hidden={true}>
+              {/*gender radio buttons*/}
+              <p>
+                <input type="radio" id="cat-both" name="catGender" value="both"/>
+                <label htmlFor="cat-both">Both</label>
+                <input type="radio" id="cat-female" name="catGender" value="female"/>
+                <label htmlFor="cat-female">Female</label>
+                <input type="radio" id="cat-male" name="catGender" value="male"/>
+                <label htmlFor="cat-male">Male</label>
+              </p>
+              {/*breed*/}
               <input className="add-option__input" placeholder="enter cat breeds separated by commas" type="text"
                      name="catBreed"/>
-            </p>
+            </div>
             <p>
-              <input type="checkbox" id="option2" name="dogs"/>
+              <input type="checkbox" id="option2" name="dogs" onChange={this.handleAnimalSelected}/>
               <label htmlFor="option2"> Dogs </label>
-              <input className="add-option__input" placeholder="enter dog breed separated by commas" type="text"
-                     name="dogBreed"/>
+              <div id="dogs" hidden={true}>
+                {/*gender radio buttons*/}
+                <p>
+                  <input type="radio" id="dog-both" name="dogGender" value="both"/>
+                  <label htmlFor="dog-both">Both</label>
+                  <input type="radio" id="dog-female" name="dogGender" value="female"/>
+                  <label htmlFor="dog-female">Female</label>
+                  <input type="radio" id="dog-male" name="dogGender" value="male"/>
+                  <label htmlFor="dog-male">Male</label>
+                </p>
+                {/*breed*/}
+                <input className="add-option__input" placeholder="enter dog breed separated by commas" type="text"
+                       name="dogBreed"/>
+              </div>
             </p>
             <p>
-              <label htmlFor="phone"> Phone number </label>
+              <label htmlFor="phone">Phone number </label>
               <input className="add-option__input" placeholder="555-555-5555" type="tel" name="phone"/>
             </p>
             <button className="button">Submit</button>
