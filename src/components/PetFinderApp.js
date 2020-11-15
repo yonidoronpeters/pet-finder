3b9bdfc0ff68785d46e627a56847e6e200180d9c
Yoni Doron-Peters <jdoronpeters@gloo.us>
2020-11-14 21:29:22 -0700

    add options for cat age and hair length

--- src/components/PetFinderApp.js
+++ src/components/PetFinderApp.js
@@ -33,12 +33,26 @@ export default class PetFinderApp extends React.Component {
   getCatOptions(e) {
     if (e.target.elements.cats.checked) {
       const gender = e.target.elements.catGender.value;
+      const ages = this.getCatValues(['kitten', 'junior', 'prime', 'mature']);
+      const hair = this.getCatValues(['short', 'medium', 'long']);
       const catBreeds = e.target.elements.catBreed.value.trim();
       return {
         gender,
+        ages,
+        hair,
         breeds: catBreeds.split(',').map(cat => cat.trim()).filter(Boolean)
       };
     }
+  }
+
+  getCatValues(values) {
+    const ages = [];
+    values.forEach(value => {
+      if (document.querySelector(`input[value="${value}"]`).checked) {
+        ages.push(value);
+      }
+    });
+    return ages;
   }
 
   validatePhoneNumber = (e) => {
@@ -74,25 +88,45 @@ export default class PetFinderApp extends React.Component {
       <div className="App">
         <header className="App-header">
           <h1>Pet Finder</h1>
-          <p>
-            Notify me for:
-          </p>
+          <h2>Notify me for:</h2>
           <form className="add-option" onSubmit={this.handleSubmitForm}>
             {/*checkbox*/}
             <input type="checkbox" id="option1" name="cats" onChange={this.handleAnimalSelected}/>
             <label htmlFor="option1"> Cats </label>
             <div id="cats" hidden={true}>
               {/*gender radio buttons*/}
-              <p>
+              <div id="cat-gender">
+                <h3>Gender</h3>
                 <input type="radio" id="cat-both" name="catGender" value="both" defaultChecked={true}/>
                 <label htmlFor="cat-both">Both</label>
                 <input type="radio" id="cat-female" name="catGender" value="female"/>
                 <label htmlFor="cat-female">Female</label>
                 <input type="radio" id="cat-male" name="catGender" value="male"/>
                 <label htmlFor="cat-male">Male</label>
-              </p>
+              </div>
+              {/*age checkbox*/}
+              <div id="cat-age">
+                <h3>Age</h3>
+                <input type="checkbox" id="cat-kitten" name="catAge" value="kitten"/>
+                <label htmlFor="cat-kitten">Less than a year</label><br/>
+                <input type="checkbox" id="cat-junior" name="catAge" value="junior"/>
+                <label htmlFor="cat-junior">1-2 years</label><br/>
+                <input type="checkbox" id="cat-prime" name="catAge" value="prime"/>
+                <label htmlFor="cat-prime">3-6 years</label><br/>
+                <input type="checkbox" id="cat-mature" name="catAge" value="mature"/>
+                <label htmlFor="cat-mature">7+ years</label>
+              </div>
               {/*breed*/}
-              <input className="add-option__input" placeholder="enter cat breeds separated by commas" type="text"
+              <div id="cat-breed">
+                <h3>Breed</h3>
+                <input type="checkbox" id="cat-short" name="catHair" value="short"/>
+                <label htmlFor="cat-short">Short hair</label><br/>
+                <input type="checkbox" id="cat-medium" name="catHair" value="medium"/>
+                <label htmlFor="cat-medium">Medium hair</label><br/>
+                <input type="checkbox" id="cat-long" name="catHair" value="long"/>
+                <label htmlFor="cat-long">Long hair</label>
+              </div>
+              <input className="add-option__input" placeholder="cat breeds separated by commas" type="text"
                      name="catBreed"/>
             </div>
             <p>
