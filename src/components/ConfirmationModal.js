87cb9296339df4f812e8c0d16bfbf443c6946d20
Yoni Doron-Peters <jdoronpeters@gloo.us>
2020-11-22 18:45:51 -0700

    Use material-ui Modal instead of react-modal

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import Modal from 'react-modal';
 import PetDisplay from './PetDisplay';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Fade from '@material-ui/core/Fade';
+import { Button } from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
-const ConfirmationModal = ({ subscription, handleCloseModal}) => (
+const ConfirmationModal = ({ subscription, handleCloseModal }) => (
   <Modal
-    isOpen={!!subscription}
-    onRequestClose={handleCloseModal}
+    open={!!subscription}
+    onClose={handleCloseModal}
     contentLabel="Pet Notification"
-    closeTimeoutMS={200}
+    closeAfterTransition
     className="modal"
   >
-    <h2 className="modal__title">You will be notified for:</h2>
-    {subscription && <div className="modal__body">
-      {subscription.dogs && <PetDisplay type="Dog" animal={subscription.dogs} />}
-      {subscription.cats &&  <PetDisplay type="Cat" animal={subscription.cats} />}
-    </div>}
+    <Fade in={!!subscription}>
+      <div className={useStyles().paper}>
+        <h2 className="modal__title">You will be notified for:</h2>
+        {subscription && <div className="modal__body">
+          {subscription.dogs && <PetDisplay type="Dog" animal={subscription.dogs}/>}
+          {subscription.cats && <PetDisplay type="Cat" animal={subscription.cats}/>}
+        </div>}
 
-    <button className="button" onClick={handleCloseModal}>OK</button>
+        <Button variant={'contained'} onClick={handleCloseModal}>OK</Button>
+      </div>
+    </Fade>
   </Modal>
 );
 
